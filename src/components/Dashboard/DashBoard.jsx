15475eb4ebc9198d689cd218369e6001f9546c60
Brian Bissette <bbissette3@yahoo.com>
2023-05-22 08:19:49 -0500

    funcitonal add topic button with cards

--- src/components/Dashboard/DashBoard.jsx
+++ src/components/Dashboard/DashBoard.jsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+//redux
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTopics } from "../../store/slice/topicSlice";
+
+//sub component
 import AddTopic from "./AddTopic";
+import TopicCard from "./TopicCard";
 
 const Dashboard = () => {
   const [showAddTopicModal, setShowAddTopicModal] = useState(false);
+  const topics = useSelector((state) => state.topics.topics);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTopics());
+  }, [dispatch]);
 
   const handleToggleAddTopicModal = () => {
     setShowAddTopicModal(!showAddTopicModal);
@@ -10,20 +23,29 @@ const Dashboard = () => {
 
   return (
     <>
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded mt-5 mx-auto block"
-        onClick={handleToggleAddTopicModal}
-      >
-        Add Topic
-      </button>
+      <div className="pl-64 pr-5">
+        <h1 className="text-4xl font-bold text-center">My Study Topics!</h1>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded mt-5 mb-5 mx-auto block"
+          onClick={handleToggleAddTopicModal}
+        >
+          Add Topic
+        </button>
 
-      {showAddTopicModal && (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 p-6">
-          <div className="bg-blue-300 rounded-lg shadow-lg">
-            <AddTopic handleToggleForm={handleToggleAddTopicModal} />
+        {showAddTopicModal && (
+          <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 p-6">
+            <div className="bg-blue-300 rounded-lg shadow-lg">
+              <AddTopic handleToggleForm={handleToggleAddTopicModal} />
+            </div>
           </div>
+        )}
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
+          {topics.map((topic) => (
+            <TopicCard key={topic.id} topic={topic} />
+          ))}
         </div>
-      )}
+      </div>
     </>
   );
 };
---
+++ src/components/Dashboard/TopicCard.jsx
@@ -0,0 +1,17 @@
+const TopicCard = ({ topic }) => {
+  return (
+    <div className="max-w-sm p-4 m-4 rounded overflow-hidden shadow-2xl bg-white transform transition-all duration-200 hover:scale-95">
+      <h2 className="text-xl font-bold text-center mb-2">{topic.title}</h2>
+      <p className="text-center mb-2">{topic.description}</p>
+      <hr />
+      <p className="text-center mb-2">
+        Resources: {topic.resources ? topic.resources.length : 0}
+      </p>
+      <p className="text-center">
+        Comments: {topic.comments ? topic.comments.length : 0}
+      </p>
+    </div>
+  );
+};
+
+export default TopicCard;
