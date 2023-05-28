d6bfa3a8d2e7bfee91fde137a667b7be2396e9ca
Brian Bissette <bbissette3@yahoo.com>
2023-05-28 18:15:28 -0500

    changed color scheme of toipc cards

--- src/components/Dashboard/TopicCard.jsx
+++ src/components/Dashboard/TopicCard.jsx
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 const TopicCard = ({ topic }) => {
   return (
     <Link to={`/topics/${topic.id}`}>
-      <div className="max-w-sm p-4 m-4 rounded overflow-hidden bg-white hover:scale-95">
+      <div className="max-w-sm p-4 m-4 rounded overflow-hidden bg-opacity-80 bg-gray-900 text-white hover:scale-95">
         <h2 className="text-2xl font-bold text-center mb-2">{topic.title}</h2>
         <p className="text-lg text-center mb-2">{topic.subject}</p>
-        <hr />
+        <hr className="border-gray-300 mb-2" />
         <p className="text-center mb-2">
-          Resources: {topic.resources ? topic.resources.length : 0}
+          <span className="text-gray-300">Resources:</span>{" "}
+          {topic.resources ? topic.resources.length : 0}
         </p>
         <p className="text-center">
-          Comments: {topic.comments ? topic.comments.length : 0}
+          <span className="text-gray-300">Comments:</span>{" "}
+          {topic.comments ? topic.comments.length : 0}
         </p>
       </div>
     </Link>
